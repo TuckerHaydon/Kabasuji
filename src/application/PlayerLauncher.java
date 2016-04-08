dffec6c21ca31e849ebf7611084d24b554e23507
tuckerhaydon <tuckerhaydon@gmail.com>
2016-04-08 11:27:19 -0400

    Created the GameWindow class and displayed the first window

--- src/application/PlayerLauncher.java
+++ src/application/PlayerLauncher.java
@@ -1,9 +1,47 @@
 package application;
 
+import playerBoundary.*;
+
 public class PlayerLauncher {
+	
+	static GameWindow gameWindow;
 	
 	public static void main(String args[])
 	{
 		System.out.println("Player Application launched.");
+		
+		gameWindow = new GameWindow();
+	}
+	
+	public void init(){
+		
+	}
+	
+	public void initModel(){
+		
+	}
+	
+	public void initView(){
+		
+	}
+	
+	public void initControllers(){
+		
+	}
+	
+	public void displayMainMenu(){
+		
+	}
+	
+	public void displayGameWindow(){
+		
+	}
+	
+	public void displayLevelSelectionMenu(){
+		
+	}
+	
+	public void displayAchievementsMenu(){
+		
 	}
 }
---
+++ src/playerBoundary/GameWindow.java
@@ -0,0 +1,13 @@
+package playerBoundary;
+
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame {
+	
+	public GameWindow()
+	{
+		super();
+		this.setSize(1000, 1000);
+	}
+
+}
