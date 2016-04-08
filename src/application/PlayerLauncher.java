1f7fc4a370e0396d56c15e2a6ba66d1a2676a25c
tuckerhaydon <tuckerhaydon@gmail.com>
2016-04-08 11:46:53 -0400

    Completed the various display windows methods and verified that they worked.

--- src/application/PlayerLauncher.java
+++ src/application/PlayerLauncher.java
@@ -1,47 +1,80 @@
 package application;
 
+import java.util.Scanner;
+
 import playerBoundary.*;
 
 public class PlayerLauncher {
 	
 	static GameWindow gameWindow;
+	static MainMenu mainMenu;
+	static LevelSelectionMenu levelSelectionMenu;
+	static AchievementsMenu achievementsMenu;
 	
 	public static void main(String args[])
 	{
 		System.out.println("Player Application launched.");
+		init();
+	}
+	
+	public static void init(){
+		initModel();
+		initView();
+		initControllers();
 		
+		/* TESTING */
+		Scanner scan = new Scanner(System.in);
+		
+		displayMainMenu();
+		scan.nextLine();
+		displayGameWindow();
+		scan.nextLine();
+		displayLevelSelectionMenu();
+		scan.nextLine();
+		displayAchievementsMenu();
+		/* TESTING */
+	}
+	
+	public static void initModel(){
+		
+	}
+	
+	public static void initView(){
 		gameWindow = new GameWindow();
+		mainMenu = new MainMenu();
+		levelSelectionMenu = new LevelSelectionMenu();
+		achievementsMenu = new AchievementsMenu();
 	}
 	
-	public void init(){
+	public static void initControllers(){
 		
 	}
 	
-	public void initModel(){
-		
+	public static void displayMainMenu(){
+		mainMenu.setVisible(true);
+		gameWindow.setVisible(false);
+		levelSelectionMenu.setVisible(false);
+		achievementsMenu.setVisible(false);
 	}
 	
-	public void initView(){
-		
+	public static void displayGameWindow(){
+		mainMenu.setVisible(false);
+		gameWindow.setVisible(true);
+		levelSelectionMenu.setVisible(false);
+		achievementsMenu.setVisible(false);
 	}
 	
-	public void initControllers(){
-		
+	public static void displayLevelSelectionMenu(){
+		mainMenu.setVisible(false);
+		gameWindow.setVisible(false);
+		levelSelectionMenu.setVisible(true);
+		achievementsMenu.setVisible(false);
 	}
 	
-	public void displayMainMenu(){
-		
-	}
-	
-	public void displayGameWindow(){
-		
-	}
-	
-	public void displayLevelSelectionMenu(){
-		
-	}
-	
-	public void displayAchievementsMenu(){
-		
+	public static void displayAchievementsMenu(){
+		mainMenu.setVisible(false);
+		gameWindow.setVisible(false);
+		levelSelectionMenu.setVisible(false);
+		achievementsMenu.setVisible(true);
 	}
 }
