f7b069bd0449c2e4896198072a88f9e98dff045d
tuckerhaydon <tuckerhaydon@gmail.com>
2016-04-08 15:55:18 -0400

    Added buttons to the main menu

--- src/playerBoundary/MainMenu.java
+++ src/playerBoundary/MainMenu.java
@@ -1,17 +1,28 @@
 package playerBoundary;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MainMenu extends JFrame {
+	
+	JButton loadGame;
+	JButton playGame;
+	JButton viewAchievements;
 	
 	public MainMenu(){
 		super();
 		this.setSize(1000,1000);
 		this.setTitle("Main Menu");
+		
+		loadGame = new JButton("Load Game");
+		playGame = new JButton("Play Game");
+		viewAchievements = new JButton("View Achievements");
 	}
 	
 	public void initView(){
-		
+		this.add(loadGame);
+		this.add(playGame);
+		this.add(viewAchievements);
 	}
 	
 	public void initControllers(){
