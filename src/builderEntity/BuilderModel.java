2295b6c8fbd2b3ea1ae6edac79c204c481eda4ef
tuckerhaydon <tuckerhaydon@gmail.com>
2016-04-09 17:21:53 -0400

    Started outline the BuilderModel class and created shell classes for Game, Level, and TileBank

--- src/builderEntity/BuilderModel.java
+++ src/builderEntity/BuilderModel.java
@@ -1,5 +1,29 @@
 package builderEntity;
 
 public class BuilderModel {
+	
+	Level level;
+	Game game;
+	String selectedBoardEltType;
+	String selectedColor;
+	int selectedNumber;
+	TileBank tb;
+	
+	public BuilderModel(){
+		this.level = null;
+		this.game = null;
+		this.selectedBoardEltType = "playable";
+		this.selectedNumber = 1;
+		this.selectedColor = "red";
+	}
+	
+	public void setlevel(Level lvl){
+		this.level = lvl;
+	}
+	
+	public void setGame(Game g){
+		this.game = g;
+	}
+	
 
 }
