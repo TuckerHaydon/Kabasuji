be99747f71d2ab906abc4e6124ed2453c2b01cc8
Damani <damani.walder@live.com>
2016-04-10 14:22:44 -0400

    Added player levelViews

--- src/playerBoundary/LevelView.java
+++ src/playerBoundary/LevelView.java
@@ -1,5 +1,18 @@
 package playerBoundary;
 
-public class LevelView {
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
+import playerEntity.Level;
+
+public class LevelView extends View {
+	BullpenView bullpenView;
+	BoardView boardView;
+	JButton goToMenu;
+	JLabel score;
+	Level level;
+	
+	public LevelView(Level l){
+		this.level = l;
+	}
 }
