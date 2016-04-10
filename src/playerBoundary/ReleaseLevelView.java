d47b837e1fb8e7eaf894570385f543d5782f7d06
Damani <damani.walder@live.com>
2016-04-10 15:25:24 -0400

    did something

--- src/playerBoundary/ReleaseLevelView.java
+++ src/playerBoundary/ReleaseLevelView.java
@@ -1,6 +1,7 @@
 package playerBoundary;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 
 import playerEntity.Level;
 import playerEntity.ReleaseLevel;
@@ -10,6 +11,16 @@ public class ReleaseLevelView extends LevelView {
 	public ReleaseLevelView(ReleaseLevel relLvl) {
 		this.level = relLvl;
 		goToMenu = new JButton("Main Menu");
+	}
+	
+	public void initView(){
+		// Set properties of the frame
+		this.setSize(1000,1000);
+		this.setTitle("Release Level");
+		
+		
+		// Add the buttons
+		this.add(goToMenu);
 	}
 
 }
