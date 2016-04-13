71bf1e21310934f021595059ae5089ee807848c9
nmdiwakar <nmdiwakar@wpi.edu>
2016-04-13 19:07:11 -0400

    added some stuff to board controller

--- src/playerController/BoardController.java
+++ src/playerController/BoardController.java
@@ -1,5 +1,19 @@
 package playerController;
 
-public class BoardController {
+import java.awt.event.MouseEvent;
+import playerBoundary.BoardView;
+import playerBoundary.KabasujiPlayerApplication;
+
+public class BoardController extends java.awt.event.MouseAdapter {
+	
+	KabasujiPlayerApplication app;
+	BoardView bv;
+	
+	public BoardController(KabasujiPlayerApplication app, BoardView bv){
+		super();
+		
+		this.app = app;
+		this.bv = bv;
+	}
 
 }
--- src/playerController/SquareController.java
+++ src/playerController/SquareController.java
@@ -25,16 +25,20 @@ public class SquareController extends java.awt.event.MouseAdapter{
 	
 	public void mouseDragged(MouseEvent me){
 		
+		//change x,y coordinates of the 
+		
 	}
 	
 	public void mousePressed(MouseEvent me){
 		
 		GameWindow gw = app.getGameWindow();
 		
+		//find the tile that is the owner of the square and set that tileview to "draggedTile"
+		
 	}
 	
 	public void mouseReleased(MouseEvent me){
-		
+	
 		GameWindow gw = app.getGameWindow();
 		
 		TileView draggingTile = gw.getDraggedTile();
