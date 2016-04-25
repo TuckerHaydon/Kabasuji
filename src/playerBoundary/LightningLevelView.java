31efc08d3e71d1a22f4f5b571f02d70cdf7b6e9c
Damani <damani.walder@live.com>
2016-04-24 23:23:41 -0400

    implemented the timer but it wont update on the GUI :(

--- src/playerBoundary/LightningLevelView.java
+++ src/playerBoundary/LightningLevelView.java
@@ -19,8 +19,8 @@ public class LightningLevelView extends LevelView{
 	JLabel timeLeft; 
 	LightningLevel level;
 	
-	//javax.swing.Timer timer;
-	//public final static int ONE_SECOND = 1000;
+	javax.swing.Timer timer;
+	public final static int ONE_SECOND = 1000;
 	
 	public LightningLevelView(KabasujiPlayerApplication app, GameModel m, LightningLevel lightningLvl) {
 		super(app, m);
@@ -53,16 +53,17 @@ public class LightningLevelView extends LevelView{
 		boardView.setBounds(25, 8*bullpenView.getSquareWidth(), 12*bullpenView.getSquareWidth(), 12*bullpenView.getSquareWidth());
 		add(boardView);
 		
-		/*
+		
 		String remTime = new String (Integer.toString(level.getRemainingTime()));
 		JLabel lblTimeLeft = new JLabel("<html>" + "Time Remaining: " + remTime + " " + "</html>");
 		lblTimeLeft.setBounds(770, 400, 60, 300);
 		add(lblTimeLeft);
-		*/
 		
-		JLabel lblTimeLeft = new JLabel("Time Allowed");
-		lblTimeLeft.setBounds(770, 770, 60, 15);
-		add(lblTimeLeft);
+		timeLeft = lblTimeLeft;
+		
+		//JLabel lblTimeLeft = new JLabel("Time Allowed");
+		//lblTimeLeft.setBounds(770, 770, 60, 15);
+		//add(lblTimeLeft);
 		
 		JLabel lblScoreNStuff = new JLabel("Score n stuff");
 		lblScoreNStuff.setBounds(770, 850, 60, 15);
@@ -72,15 +73,23 @@ public class LightningLevelView extends LevelView{
 		
 	}
 	
+	public Timer getTimer(){
+		return timer;
+	}
+	
 	@Override
 	public void initControllers(){
 		// Init the controllers of the subcomponents
 		bullpenView.initControllers();
 		boardView.initControllers();
 		
-		//timer = new Timer(ONE_SECOND, new LightningLevelTimer(level));
+		timer = new Timer(ONE_SECOND, new LightningLevelTimer(level, app));
 		
 		// Init own controllers
 		setMouseAdapter(new LevelController(app, m, app.getGameWindow().getLevelView()));
+	}
+	
+	public JLabel getJLabel(){
+		return timeLeft;
 	}
 }
