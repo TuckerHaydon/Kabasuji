c2df41bf13666f5e5e8bfd25f015c1f8ea83a60d
Dorothy <jhu4@wpi.edu>
2016-04-28 17:40:36 -0400

    GUI stuff

--- src/playerBoundary/InstructionsPage.java
+++ src/playerBoundary/InstructionsPage.java
@@ -1,5 +1,7 @@
 package playerBoundary;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -32,13 +34,35 @@ public class InstructionsPage extends JFrame {
 		// Set properties of the frame
 		this.setSize(1000,1000);
 		
+		this.getContentPane().setBackground(new Color(255, 228, 225));
+		
 		ArrayList<JLabel> instructions = new ArrayList<>();
 		
-		instructions.add(new JLabel("Hello Friend!"));
-		instructions.add(new JLabel("Welcome to Kabasuji"));
-		instructions.add(new JLabel("To rotate a tile, hold control and either right-click or left-click on a tile in the bullpen."));
-		instructions.add(new JLabel("To mirror a tile, hold shift and either right-click or left-click on a tile in the bullpen."));
 		
+		
+		JLabel hellofriend= new JLabel("Hello Friend!");
+		hellofriend.setFont(new Font("Consolas", Font.PLAIN, 16));
+		instructions.add(hellofriend);
+		
+		JLabel welcome= new JLabel("Welcome to Kabasuji =^¦Ø^=");
+		welcome.setFont(new Font("Consolas", Font.PLAIN, 16));
+		instructions.add(welcome);
+		
+		JLabel torotate= new JLabel("To rotate a tile:");
+		torotate.setFont(new Font("Consolas", Font.PLAIN, 16));
+		instructions.add(torotate);
+		
+		JLabel holdcontrol= new JLabel("    Hold control and either right-click or left-click on a tile in the bullpen.");
+		holdcontrol.setFont(new Font("Consolas", Font.PLAIN, 16));
+		instructions.add(holdcontrol);
+		
+		JLabel tomirror= new JLabel("To mirror a tile:");
+		tomirror.setFont(new Font("Consolas", Font.PLAIN, 16));
+		instructions.add(tomirror);
+		
+		JLabel holdshift= new JLabel("    Hold shift and either right-click or left-click on a tile in the bullpen.");
+		holdshift.setFont(new Font("Consolas", Font.PLAIN, 16));
+		instructions.add(holdshift);
 		
 		
 		for(int i = 0; i < instructions.size(); i++){
@@ -46,7 +70,9 @@ public class InstructionsPage extends JFrame {
 			this.add(instructions.get(i));
 		}
 		
-		mainMenu.setBounds(0, 0, 200, 100);
+		mainMenu.setBounds(12, 13, 190, 47);
+		mainMenu.setBackground(new Color(128, 128, 128));
+		mainMenu.setFont(new Font("Snap ITC", Font.PLAIN, 16));
 		this.add(mainMenu);
 		
 	}
