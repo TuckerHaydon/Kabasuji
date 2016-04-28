13b3b65451ff8a910881befa56a875a5cec176de
tuckerhaydon <tuckerhaydon@gmail.com>
2016-04-28 12:02:20 -0400

    Added an intro song

---
+++ src/playerBoundary/InstructionsPage.java
@@ -0,0 +1,20 @@
+package playerBoundary;
+
+import javax.swing.JFrame;
+
+public class InstructionsPage extends JFrame {
+	
+	public InstructionsPage(){
+		super();
+		
+	}
+	
+	public void initView(){
+		
+		
+		setResizable(false);
+		// Set properties of the frame
+		this.setSize(1000,1000);
+	}
+
+}
