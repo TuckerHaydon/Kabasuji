3f3e8e534852e305fce95f4c7dec50777b700271
Joseph Wilder <wilderj715@gmail.com>
2016-04-29 14:48:19 -0400

    reorganized EditGameHandler and EditLevelHandler, started writing test cases for both

---
+++ test/builderController/testEditLevelHandler.java
@@ -0,0 +1,27 @@
+package builderController;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import builderBoundary.KabasujiBuilderApplication;
+import builderEntity.BuilderModel;
+
+public class testEditLevelHandler {
+
+	@Test
+	public void test() {
+		BuilderModel m = BuilderModel.instance();
+		KabasujiBuilderApplication builder = new KabasujiBuilderApplication(m);
+		builder.init();
+		
+		EditLevelHandler elh = new EditLevelHandler(builder, m);
+		
+		ArrayList<String> levelNames = elh.addLevels();
+		elh.parseLevel("Level 11");
+		//elh.processAction();
+	}
+
+}
