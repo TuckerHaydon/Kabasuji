46886f4704544e5d92fe0bb4381760112fb0c47c
tuckerhaydon <tuckerhaydon@gmail.com>
2016-05-03 12:25:25 -0400

    Comments and stuff

--- src/builderController/NavigateInstructionsPage.java
+++ src/builderController/NavigateInstructionsPage.java
@@ -5,20 +5,35 @@ import java.awt.event.ActionListener;
 
 import builderBoundary.KabasujiBuilderApplication;
 
+/**
+ * Controller class that handles navigating to the instructions menu in the builder
+ * @author tuckerhaydon
+ *
+ */
 public class NavigateInstructionsPage implements ActionListener{
 
 	KabasujiBuilderApplication app;
 	
+	/**
+	 * Constructor
+	 * @param app - The top-level application
+	 */
 	public NavigateInstructionsPage(KabasujiBuilderApplication app) {
 		this.app = app;
 	}
 
+	/**
+	 * This method is called when a button is pressed
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		processAction();
 		
 	}
 	
+	/**
+	 * Displays the instructions page
+	 */
 	void processAction(){
 		app.displayInstructionsPage();
 	}
